0de71ad058b0d3695ca1fb58099dd7c09ed0c55d
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-01-19 15:36:09 +0700

    lab12.1*edit
    Signed-off-by: NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[])
+{
+    double sum = 0,sum1 = 0,sum2 = 1,sum3 = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+    b[0] = sum/n;
+    for(int i = 0; i < n; i++)
+    {
+        sum1 += pow(a[i] - b[0],2);
+    }
+    b[1] = sqrt(sum1/n);
+    for(int i = 0; i < n; i++)
+    {
+        sum2 *= a[i];
+    }
+    b[2] = pow(sum2,(1.0/n));
+    for(int i = 0; i < n; i++)
+    {
+        sum3 += (1/a[i]);
+    }
+    b[3] = n/sum3;
+    double min = a[0],max = a[0];
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] > max) max = a[i];
+        else if(a[i] < min) min = a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+}
